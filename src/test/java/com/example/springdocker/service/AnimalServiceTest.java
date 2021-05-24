45c6f02767001a0192d6b7b9e77a3eca89102874
Nick Danielsson <nickdanielsson87@gmail.com>
2021-05-24 09:52:27 +0200

    greeter

--- src/test/java/com/example/springdocker/service/AnimalServiceTest.java
+++ src/test/java/com/example/springdocker/service/AnimalServiceTest.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-@DataMongoTest
 @ExtendWith(MockitoExtension.class)
 class AnimalServiceTest {
 
